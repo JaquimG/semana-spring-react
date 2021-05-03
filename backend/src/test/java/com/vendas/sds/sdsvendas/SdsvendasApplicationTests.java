4178049061e047452002b4a7b66d66c275f5f0b2
JaquimG <joaquimdias123@hotmail.com>
2021-05-03 20:02:41 -0300

    project clean

---
+++ backend/src/test/java/com/vendas/sds/sdsvendas/SdsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vendas.sds.sdsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
